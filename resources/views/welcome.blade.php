ec92e9302e64f95d6d43a289830d9730ddb56623
Dhanya16 <117178131+Dhanya16@users.noreply.github.com>
2023-10-22 18:50:33 +0530

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
         <!--CSS files-->
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/loader.css" rel="stylesheet">
+        <link href="css/header.css" rel="stylesheet">
         <!--CSS files ends-->
     </head>
     <body>
@@ -22,6 +23,49 @@
         <!--Loader ends-->
 
         <!--Header section-->
+        <header>
+        <nav class="navbar navbar-expand-lg navbar-custom">
+        <div class="container">
+            <a class="navbar-brand" href="login" >
+                <img src="images/logo.png" alt="Logo" width="50" height="50" class="logo">
+                CampusBite
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="login">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="categoriesDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Categories
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="categoriesDropdown">
+                            <a class="dropdown-item" href="login">Category 1</a>
+                            <a class="dropdown-item" href="login">Category 2</a>
+                            <a class="dropdown-item" href="login">Category 3</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login">Order</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login">Reviews</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn mr-2 signup" href="register">Sign Up</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn login" href="login">Log In</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
         <!--Header section ends-->
 
         <!--Hero section-->
